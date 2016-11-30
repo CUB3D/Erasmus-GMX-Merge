fa8f29b05978246b77678117737e6f5c3ba1919d
CUB3D <callum.thomson@discoveryschool.org.uk>
2016-11-30 15:18:43 +0000

    Added comments to Main.py

--- Main.py
+++ Main.py
@@ -8,12 +8,20 @@ def getXmlDict(file):
     return xmlDict
 
 def loadConfigFiles(project):
+    """
+    Loads the data from the config files into the project
+    :param project: The project object
+    """
     for child in project["Configs"]["children"]:
         configPath = project.expandPath(child["content"]) + ".config.gmx" # expands the path
         print("Loading path:", configPath)
         project.configs.append(getXmlDict(configPath)) #appends all the returned config files in the directory
 
 def loadObjectFiles(project):
+    """
+    Loads the data from the object files into the project
+    :param project: The project object
+    """
     for child in project["objects"]["children"]:
         if child["name"] != "object":#a quick get around for if the data is split into sub groups
             for Baby in child["children"]:#iterates through all the children of the children
@@ -25,23 +33,41 @@ def loadObjectFiles(project):
         project.objects.append(getXmlDict(configPath))
 
 def loadScriptFiles(project):
+    """
+    Loads the data from the scripts into a list of script objects in the project
+    :param project: The project object
+    """
     for child in project["scripts"]["children"]:
         configPath = project.expandPath(child["content"]) + ".gml"
         print("Loading path:", configPath)
         project.scripts.append(Script(child["content"], project))
+
 def loadRoomFiles(project):
+    """
+    Loads the data from the room files into the project
+    :param project: The project object
+    """
     for child in project["rooms"]["children"]:
         configPath = project.expandPath(child["content"]) + ".room.gmx"
         print("Loading path:", configPath)
         project.rooms.append(getXmlDict(configPath))
 
 def loadSpriteFiles(project):
+    """
+    Loads sprite data from the sprite files into the project
+    :param project: The project object
+    """
     for child in project["sprites"]["children"]:
         configPath = project.expandPath(child["content"]) + ".sprite.gmx"
         print("Loading path:", configPath)
         project.sprites.append(getXmlDict(configPath))
 
 def parseProjectData(file):
+    """
+    Loads the data from the project into a project object and builds
+    resolution tables from this data
+    :param file: The path to the root diretory in the project
+    """
     project = gameMakerProject(file)#Create an instance of the game maker project type using the passed file location as the root
     project.project = getXmlDict(project.expandPath(project.projectName + ".project.gmx")) #concatanate ".project.gmx" using the Expand path function and then parse it using the XML parser from XMLstuff
     print("Loading and parsing config")
@@ -56,6 +82,7 @@ def parseProjectData(file):
     loadSpriteFiles(project)
     project.buildResolutionTable()
     return project
+
 project1 = parseProjectData("./Examples/Erasmus.gmx")#Start using the file Erasmus in the example
 project2 = parseProjectData("./Examples/FireWorldScales.gmx")#throws error as not finding file
 input()#hang
